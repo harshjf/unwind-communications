5f60a880f620bc871850dcb3729e1397486bd4bc
harshjf <harsh@juniperforge.com>
2024-12-30 12:06:12 +0530

    WIP changes

--- src/components/Instagram/Banner2.js
+++ src/components/Instagram/Banner2.js
@@ -22,73 +22,8 @@ const Banner2 = () => {
 
   return (
     <>
-      {/*  <div className="banner-container">
-        <div className="container-image">
-        
-          <div className="image-container">
-            <img
-              src="/images/backgroundbanner.jpg"
-              className="curved-image"
-              alt="Background"
-            />
-            <div className="rotating-image-container">
-              <img
-                src="/images/Heading.png"
-                alt="Heading Image"
-                width={200}
-                height={200}
-                className="rotating-image"
-              />
-
-              <div className="play-button-container">
-                <img
-                  src="/images/play-button.png"
-                  alt="Play Button"
-                  width={50}
-                  height={50}
-                  className="play-button"
-                  style={{ cursor: "pointer" }}
-                  onClick={() =>
-                    window.open(
-                      "https://youtu.be/mwEZkRvjRC8?si=M9z5IJxbbNbFfUWZ"
-                    )
-                  }
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="banner-text-container">
-          <h1
-            className="banner-one__title-one"
-            data-aos="fade-right"
-            data-aos-once="false"
-          >
-            UNWIND <span style={{ color: "#fff" }}>COMMUNICATIONS</span>
-          </h1>
-          <p
-            className="banner-description"
-            data-aos="fade-up"
-            data-aos-once="false"
-            style={{ textAlign: "left" }}
-          >
-            We bring India&apos;s most captivating destinations and venues to
-            life on digital platforms, creating a visual journey that resonates
-            across the globe. At UNWIND COMMUNICATIONS, we redefine experiential
-            marketing, led by the extraordinary Award Winning ARTISTPRENEUR,
-            Shefali Saxena, whose work has set new benchmarks in the industry.
-          </p>
-        </div>
-        <div className="banner1-shape2">
-          <img
-            src="/images/banner1-shape5.png"
-            alt="Animated Shape"
-            style={{ width: "150px", height: "150px" }}
-          />
-        </div>
-      </div> */}
       <div className="banner3-container">
-        <div className="banner3-overlay"></div>
+        <div className="banner3-overlay" />
         <div className="banner3-inner">
           <div className="banner3-left">
             <h1
@@ -96,7 +31,7 @@ const Banner2 = () => {
               data-aos="fade-right"
               data-aos-once="false"
             >
-              <span style={{ color: "#973d5d" }}>UNWIND</span>{" "}
+              <span style={{ color: "#973d5d" }}>UNWIND</span> <br />
               <span style={{ color: "#fff" }}>COMMUNICATIONS</span>
             </h1>
             <h1
@@ -127,15 +62,29 @@ const Banner2 = () => {
               {" "}
             </div>
 
-            <iframe
+            {/* <iframe
               className="banner3-video"
-              /*  src="https://www.youtube.com/embed/_wGWmbJpSVQ?autoplay=1&mute=1&loop=1&playlist=_wGWmbJpSVQ&cc_load_policy=0" */
+              // src="https://www.youtube.com/embed/_wGWmbJpSVQ?autoplay=1&mute=1&loop=1&playlist=_wGWmbJpSVQ&cc_load_policy=0"
               src="https://www.youtube.com/embed/_wGWmbJpSVQ?autoplay=1&mute=1&controls=0&showinfo=0&modestbranding=1&loop=1&playlist=_wGWmbJpSVQ&cc_load_policy=0"
               title="Unwind Communication Video"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
               style={{ position: "relative", zIndex: 1 }}
-            ></iframe>
+            /> */}
+
+            <video
+              className="banner3-video"
+              src="/videos/banner_video.mp4"
+              autoPlay
+              loop
+              muted
+              playsInline
+              style={{
+                width: "100%",
+                height: "auto",
+                objectFit: "cover",
+              }}
+            />
 
             {/*   <YouTube videoId="_wGWmbJpSVQ" opts={opts} /> */}
           </div>
